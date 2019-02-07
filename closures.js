adde15a0ed27c2f252bc3e2c68ed134aa3486375
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-07 13:36:59 -0800

    starting stretch exercise

--- closures.js
+++ closures.js
@@ -40,10 +40,31 @@ var countdownGenerator = function (x) {
   }
 };
 
-var countdown = countdownGenerator(3);
-countdown(); // T-minus 3...
-countdown(); // T-minus 2...
-countdown(); // T-minus 1...
-countdown(); // Blast Off!
-countdown(); // Rockets already gone, bub!
-countdown(); // Rockets already gone, bub!
+// var countdown = countdownGenerator(3);
+// countdown(); // T-minus 3...
+// countdown(); // T-minus 2...
+// countdown(); // T-minus 1...
+// countdown(); // Blast Off!
+// countdown(); // Rockets already gone, bub!
+// countdown(); // Rockets already gone, bub!
+
+// higher order function stuff
+var wrapLog = function (callback, name) {
+  /* your code here */
+};
+
+var area = function (x, y) {
+  return x * y;
+};
+var logArea = wrapLog(area, "area");
+
+logArea(5, 3); // area(5, 3) => 15
+logArea(3, 2); // area(3, 2) => 6
+
+var volume = function (x, y, z) {
+  return x * y * z;
+};
+var logVolume = wrapLog(volume, "volume");
+
+logVolume(5, 3, 2); // volume(5, 3, 2) => 30
+logVolume(3, 2, 4); // volume(3, 2, 4) => 24
