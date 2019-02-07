f21d30caebb64e65552056fbbe608adf708cf69d
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-07 13:18:40 -0800

    finalized loadedDie, working on countdown

--- closures.js
+++ closures.js
@@ -2,19 +2,41 @@ var rollDie = function () {
   return Math.floor(1 + Math.random() * 6);
 }
 
-console.log(rollDie()); // 1 (for example)
+// console.log(rollDie()); // 1 (for example)
 
+// fakes random dice rolls by reading from list
 function makeLoadedDie() {
   var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
-  /* your code here */
+  var i = -1;
 
   return function () {
-    /* your code here */
+    i++;
+    return list[i];
   }
 }
 
 var rollLoadedDie = makeLoadedDie();
 
-console.log(rollLoadedDie()); // 5
-console.log(rollLoadedDie()); // 4
-console.log(rollLoadedDie()); // 6
+// console.log(rollLoadedDie()); // 5
+// console.log(rollLoadedDie()); // 4
+// console.log(rollLoadedDie()); // 6
+
+// counts down from num passed through
+var countdownGenerator = function (x) {
+  /* your code here */
+  var count = x;
+
+  return count => {
+    if (count > 0) {
+      console.log("T-minus ${count}");
+    }
+  }
+};
+
+var countdown = countdownGenerator(3);
+countdown(); // T-minus 3...
+countdown(); // T-minus 2...
+countdown(); // T-minus 1...
+countdown(); // Blast Off!
+countdown(); // Rockets already gone, bub!
+countdown(); // Rockets already gone, bub!
