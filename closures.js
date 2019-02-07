c6f9e613cf4128d37f7d8b97b4a9cfb34ef3df02
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-07 13:45:46 -0800

    finalized callback + higher order question

--- closures.js
+++ closures.js
@@ -49,8 +49,16 @@ var countdownGenerator = function (x) {
 // countdown(); // Rockets already gone, bub!
 
 // higher order function stuff
+// Create a function called wrapLog() that takes in a function (callback) and a string (name) 
+// and returns a function that internally invokes (calls) callback during its execution and 
+// also logs the name, input parameters, and return value of the callback function. 
 var wrapLog = function (callback, name) {
   /* your code here */
+  var operation = name;
+
+  return function (x, y, z) {
+    return callback(x, y, z);
+  }
 };
 
 var area = function (x, y) {
@@ -58,7 +66,7 @@ var area = function (x, y) {
 };
 var logArea = wrapLog(area, "area");
 
-logArea(5, 3); // area(5, 3) => 15
+console.log(logArea(5, 3)); // area(5, 3) => 15
 logArea(3, 2); // area(3, 2) => 6
 
 var volume = function (x, y, z) {
@@ -66,5 +74,5 @@ var volume = function (x, y, z) {
 };
 var logVolume = wrapLog(volume, "volume");
 
-logVolume(5, 3, 2); // volume(5, 3, 2) => 30
+console.log(logVolume(5, 3, 2)); // volume(5, 3, 2) => 30
 logVolume(3, 2, 4); // volume(3, 2, 4) => 24
